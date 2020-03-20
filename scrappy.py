c7e098bb2e059ff4937b59ff9e785a0682391ce9
ShadowCrackerStudio <62294716+ShadowCrackerStudio@users.noreply.github.com>
2020-03-20 19:26:25 +0100

    Update scrappy.py

--- scrappy.py
+++ scrappy.py
@@ -456,6 +456,6 @@ async def info(ctx):
 
     await ctx.send(embed=embed)
                    
-bot.run ("Njg5NDY1MjEwNTgyNTk3NzAx.XnO6uQ.D7wujP7C7DN7SCwxwFb6jkrWtz8")
+bot.run ("NjkwNjI3MTQyMzc1MTc4MzQx.XnUKrg.6LoufDCyfxClF0WfXtuPmtFFyCg")
 
 
